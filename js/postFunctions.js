040ff6e4b98f0ea8370fb388bfaf63964c721324
Fedhira Syaila <pedirasyaila@gmail.com>
2023-10-22 10:42:45 +0700

    pushh

--- js/postFunctions.js
+++ js/postFunctions.js
@@ -1,6 +1,6 @@
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
-// import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
+import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
 export default function PostSignUp() {
     let target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/Billblis";
