a74412eef37f481bcaaaaadae90e6222b3175be4
Fedhira Syaila <pedirasyaila@gmail.com>
2023-11-14 19:39:18 +0700

    update alert login

--- assets/js/postFunctions.js
+++ assets/js/postFunctions.js
@@ -1,7 +1,8 @@
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
 import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
-import Swal from './sweetalert2.min.js';
+import './sweetalert2.min.js';
+import '../css/sweetalert2.min.css';
 
 export default function PostSignUp() {
     let target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/Billblis";
