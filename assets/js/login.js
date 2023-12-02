d6b9580d5ab58c521af662d656a54aba089cd805
Marlina Lubis <lubismarlina5@gmail.com>
2023-12-02 21:44:07 +0700

    update login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,10 @@
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
-import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
+import { getValue } from "https://jscroot.github.io/element/croot.js";
 import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
 export default function PostSignIn() {
     let target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/Billblis";
-    // let tokenkey = "token";
+    let tokenkey = "token";
     // let tokenvalue = "5f2dcd0e6f39ad4515c8397819a04a22bd6ff03d63b0eaa5913c9e93a217c33b";
     let datainjson = {
         "name": getValue("name"),
@@ -12,7 +12,8 @@ export default function PostSignIn() {
     }
 
     // postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
-    postWithToken(target_url,datainjson,responseData);
+    // postWithToken(target_url,datainjson,responseData);
+    postWithToken(target_url,'Authorization', 'Bearer' + tokenkey, datainjson,responseData);
 }
 
 function responseData(result) {
@@ -40,4 +41,6 @@ function responseData(result) {
     }
 }
 
-window.PostSignIn = PostSignIn;
+document.getElementById("button").addEventListener("click", PostSignIn)
+
+// window.PostSignIn = PostSignIn;
