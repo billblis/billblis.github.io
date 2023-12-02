f623b6732f36e7ea56d1ce0f780a8965fe52283e
Marlina Lubis <lubismarlina5@gmail.com>
2023-12-02 23:49:08 +0700

    update login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,10 @@
-import { post } from "https://jscroot.github.io/api/croot.js";
+import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 
 export default function PostSignIn() {
     let target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/login";
-    // let tokenkey = "token";
+    let token = "token";
     // let tokenvalue = "5f2dcd0e6f39ad4515c8397819a04a22bd6ff03d63b0eaa5913c9e93a217c33b";
     let datainjson = {
         "username": getValue("username"),
@@ -12,7 +12,7 @@ export default function PostSignIn() {
     }
 
     // postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
-    post(target_url, datainjson, responseData);
+    postWithToken(target_url, token, datainjson, responseData);
 }
 
 function responseData(result) {
@@ -40,4 +40,5 @@ function responseData(result) {
     }
 }
 
-window.PostSignIn = PostSignIn;
+document.getElementById("button").addEventListener("click", PostSignIn);
+// window.PostSignIn = PostSignIn;
