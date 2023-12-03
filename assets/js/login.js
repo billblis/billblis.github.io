c10f7a8cb137ea6a0b3767bc229a284d566dd4ae
Marlina Lubis <lubismarlina5@gmail.com>
2023-12-03 10:57:23 +0700

    Update login

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,8 +7,8 @@ export default function PostSignIn() {
     // let token = "token";
     // let tokenvalue = "5f2dcd0e6f39ad4515c8397819a04a22bd6ff03d63b0eaa5913c9e93a217c33b";
     let datainjson = {
-        "username": getValue("username"),
-        "password": getValue("password")
+        username: getValue("username"),
+        password: getValue("password")
     }
 
     // postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
@@ -17,7 +17,7 @@ export default function PostSignIn() {
 
 function responseData(result) {
     if (result.message == "Selamat Datang") {
-        setCookieWithExpireHour("token", result.token, 2);
+        setCookieWithExpireHour("Authorization", result.token, 2);
 
         // Use SweetAlert for success message
         Swal.fire({
@@ -34,8 +34,6 @@ function responseData(result) {
             icon: 'error',
             title: 'Gagal Masuk',
             text: result.message,
-        }).then(() => {
-            console.log(result.message);
         });
     }
 }
