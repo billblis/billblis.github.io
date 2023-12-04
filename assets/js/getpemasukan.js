644f5de6f5069ce50984b305f97a6f97206e1a88
Marlina Lubis <lubismarlina5@gmail.com>
2023-12-04 13:46:55 +0700

    update tambah pemasukan

--- assets/js/getpemasukan.js
+++ assets/js/getpemasukan.js
@@ -1,91 +1,28 @@
-import { postWithBearer } from "../js/utilities/api.js";
-import {getCookie } from "https://jscroot.github.io/cookie/croot.js";
-import { addInner } from "https://jscroot.github.io/element/croot.js"
-// import { showLoadingModal, hideLoadingModal } from "./utilities/loading.js"
-// import { tabelTopic } from "./temp/table.js";
+// Function to get all income data
+function getAllPemasukan() {
+    try {
+        const response =  fetch('https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/getAllPemasukan');
 
-const Postdata = () => {
-    const target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/getAllPemasukan";
-
-    let id = false;
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has('id')) {
-        if (urlParams.get('id') != '') {
-            id = urlParams.get('id');
-        }
-    }
-    const datainjson = {
-        "_id": id
-    };
-    const token = getCookie("token")
-    if (token) {
-        postWithBearer(target_url, token, datainjson, responseData)
-    } else {
-        console.log("token tidak ada " + result.message);
-        alert("sesi anda sudah habis, silahkan logout dan login ulang")
-        window.location.href = "pemasukan.html";
-
-    }
-    // post(target_url, datainjson, responseData);
-};
-
-const responseData = (result) => {
-    // console.log(result);
-    if (result.status === true) {
-
-        addInner("tanggal_masuk", result.data[0].tanggal_masuk)
-        addInner("rupiah", result.data[0].rupiah)
-        addInner("sumber", result.data[0].sumber)
-        addInner("deskripsi", result.data[0].deskripsi)
-
-        document.getElementById("tanggal_masuk_Input").value = result.data[0].tanggal_masuk;
-        document.getElementById("rupiahInput").value = result.data[0].jumlah_masuk;
-        document.getElementById("sumber_Input").value = result.data[0].jumlah_masuk;
-        document.getElementById("deskripsi_Input").value = result.data[0].deskripsi;
-        
-
-
-        const defaultValue = result.data[0].source.source; // Change this value as needed
-        document.querySelector(`input[type="radio"][value="${defaultValue}"]`).checked = true;
-
-        //isi tabel
-        if (result.data[0].status == "inputting") {
-
-            document.getElementById("actionButton").innerHTML = "Tambah Pemasukan";
-            document.getElementById("actionButton").setAttribute("onclick", "tambah()");
-            document.getElementById("actionButton").setAttribute("class", "button is-primary");
-
-            if (result.datapemasukan.length > 0) {
-                let index = 0;
-                let isiRow = (value) => {
-                    console.log(value)
-                    let content =
-                        tabelTopic.replace("#NO#", index += 1)
-                            .replace("#TANGGAL#", value.tanggal_masuk)
-                            .replace("#JUMLAH#", value.jumlah_masuk)
-                            .replace("#SUMBER#", value.sumber)
-                            .replace("#DESKRIPSI#", value.deskripsi)
-                    addInner("isiTabel", content);
-                }
-
-                result.datatopics.forEach(isiRow)
-
-                document.getElementById("textNoData").style.display = "none";
-            }
-        } else if(result.data[0].status == "Tambah") {
-            document.getElementById("actionButton").innerHTML = "Data Pemasukan";
-            document.getElementById("yourElementId").removeAttribute("onclick");
-            document.getElementById("actionButton").setAttribute("class", "button is-primary");
-            document.getElementById("actionButton").setAttribute("disabled", true);
+        if (!response.ok) {
+            throw new Error('Failed to fetch income data');
         }
 
-        // window.location.href = "sentimen.html";
-    } else {
-        console.log(result.message);
-        alert(`parameter bermasalah atau sesi anda sudah habis, silahkan ulangi atau logout dan login ulang`);
-        window.location.href = "pemasukan.html";
-
+        const pemasukanData =  response.json();
+        console.log('All Pemasukan:', pemasukanData);
+        return pemasukanData;
+    } catch (error) {
+        console.error('Error fetching income data:', error);
+        return null;
     }
-};
+}
 
-Postdata();
+// Example usage
+document.getElementById("getAllPemasukanButton").addEventListener("click", getAllPemasukan);
+// document.getElementById("getAllPemasukanButton").addEventListener("click",  function () {
+//     const allPemasukan = getAllPemasukan();
+
+//     if (allPemasukan !== null) {
+//         // Do something with the retrieved income data if needed
+//         console.log("All Pemasukan:", allPemasukan);
+//     }
+// });
--- assets/js/insertpemasukan.js
+++ assets/js/insertpemasukan.js
@@ -1,95 +1,44 @@
-import { postWithBearer } from "../js/utilities/api.js";
-import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
-import { getValue } from "https://jscroot.github.io/element/croot.js"
+// Function to insert income data
+function insertPemasukan(tanggal_masuk, jumlah_masuk, sumber, deskripsi) {
+    try {
+        const response = fetch('https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/insertPemasukan', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                tanggal_masuk: tanggal_masuk,
+                jumlah_masuk: jumlah_masuk,
+                sumber: sumber,
+                deskripsi: deskripsi,
+            }),
+        });
 
-const PostinsertPemasukan = () => {
-    const radioButtons = document.querySelectorAll('input[name="radioOption"]');
-    let selectedValue;
-
-    radioButtons.forEach(radioButton => {
-        if (radioButton.checked) {
-            selectedValue = radioButton.value;
+        if (!response.ok) {
+            throw new Error('Failed to insert income data');
         }
-    });
 
-    //validasi form
-    if (getValue("tanggalmasukInput") === ""|| getValue("tanggalmasukInput") === null) {
-        alert("Tanggal tidak boleh kosong");
-        return;
-    }else if (getValue("rupiahInput") === ""|| getValue("rupiahInput") === null) {
-        alert("Jumlah masuk tidak boleh kosong");
-        return;
-    }else if (getValue("sumberInput") === ""|| getValue("sumberInput") === null) {
-        alert("Sumber tidak boleh kosong");
-        return;
-    }else if (getValue("deskripsiInput") === ""|| getValue("deskripsiInput") === null) {
-        alert("Deskripsi tidak boleh kosong");
-        return;
-    } else if (selectedValue === "" || selectedValue === null) {
-        alert("Deskripsi tidak boleh kosong");
-        return;
+        const result = response.json();
+        console.log('Inserted ID:', result.insertedID);
+        return result.insertedID;
+    } catch (error) {
+        console.error('Error inserting income data:', error);
+        return null;
     }
+}
 
-    const target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/insertPemasukan";
-    const datainjson = {
-        tanggal_masuk: getValue("tanggalmasukinput"),
-        jumlah_masuk: getValue("rupiahInput"),
-        sumber: getValue("sumberInput"),
-        deskripsi: getValue("deskripsiInput"),
-        source: {
-            source: selectedValue,
-            value: getValue("pemasukan")
-        }
-    };
-    const token = getCookie("token")
-    if (token) {
-        postWithBearer(target_url, token, datainjson, responseData)
-        alert("Berhasil menambahkan topik")
-    } else {
-        console.log("token tidak ada " + result.message);
-        alert("sesi anda sudah habis, silahkan logout dan login ulang")
-    }
+// Example usage
+document.getElementById("submitIncomeButton").addEventListener("click", insertPemasukan);
+// document.getElementById("submitIncomeButton").addEventListener("click",  function () {
+//     const tanggal_masuk = eventDateInput.value; // Assuming eventDateInput is defined
+//     const jumlah_masuk = parseInt(document.getElementById("jumlahMasuk").value); // Assuming jumlahMasuk is the ID for the input field
+//     const sumber = document.getElementById("sumber").value; // Assuming sumber is the ID for the input field
+//     const deskripsi = document.getElementById("deskripsi").value; // Assuming deskripsi is the ID for the input field
 
-    const addButton = document.createElement("button");
-    addButton.id = "button";
-    addButton.type = "button";
-    addButton.className = "mr-4 px-5 py-3 font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-purple";
-    addButton.onclick = tambahData;
-    addButton.innerText = "Tambah";
+//     const insertedID =  insertPemasukan(tanggal_masuk, jumlah_masuk, sumber, deskripsi);
 
-    const cancelButton = document.createElement("button");
-    cancelButton.id = "button";
-    cancelButton.type = "button";
-    cancelButton.className = "px-5 py-3 font-medium leading-5 text-white transition-colors duration-150 bg-red-600 border border-transparent rounded-lg active:bg-red-600 hover:bg-red-700 focus:outline-none focus:shadow-outline-red";
-    cancelButton.onclick = batalTambahData;
-    cancelButton.innerText = "Batal";
-
-    document.body.appendChild(addButton);
-    document.body.appendChild(cancelButton);
-};
-
-
-
-const responseData = (result) => {
-    // console.log(result);
-    if (result.status === true) {
-        window.location.href = "pemasukan.html";
-    } else {
-        console.log(result.message);
-        alert(`sesi anda sudah habis, silahkan logout dan login ulang`);
-    }
-};
-
-const tambahData = () => {
-    // Logika untuk menangani tombol "Tambah"
-    console.log("Menambahkan data");
-};
-
-const batalTambahData = () => {
-    // Logika untuk menangani tombol "Batal"
-    console.log("Membatalkan penambahan data");
-};
-
-
-
-export default PostinsertPemasukan;
+//     if (insertedID !== null) {
+//         // Do something with the inserted ID if needed
+//         console.log("Inserted ID:", insertedID);
+//     }
+// });
