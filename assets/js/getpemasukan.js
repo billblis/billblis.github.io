7252c6159a2c47be67cad4049588a407f4983cd4
Fedhira Syaila <pedirasyaila@gmail.com>
2023-12-04 16:59:41 +0700

    update insert pemasukan

--- assets/js/getpemasukan.js
+++ assets/js/getpemasukan.js
@@ -1,91 +1,130 @@
 import { postWithBearer } from "../utilities/api.js";
 import {getCookie } from "https://jscroot.github.io/cookie/croot.js";
 import { addInner } from "https://jscroot.github.io/element/croot.js"
-// import { showLoadingModal, hideLoadingModal } from "./utilities/loading.js"
-// import { tabelTopic } from "./temp/table.js";
+// // import { showLoadingModal, hideLoadingModal } from "./utilities/loading.js"
+// // import { tabelTopic } from "./temp/table.js";
 
-const Postdata = () => {
-    const target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/getAllPemasukan";
+function getWithToken(target_url, responseFunction) {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", getCookie("Authorization"));
 
-    let id = false;
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.has('id')) {
-        if (urlParams.get('id') != '') {
-            id = urlParams.get('id');
-        }
-    }
-    const datainjson = {
-        "_id": id
+    const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
     };
-    const token = getCookie("token")
-    if (token) {
-        postWithBearer(target_url, token, datainjson, responseData)
-    } else {
-        console.log("token tidak ada " + result.message);
-        alert("sesi anda sudah habis, silahkan logout dan login ulang")
-        window.location.href = "pemasukan.html";
 
-    }
-    // post(target_url, datainjson, responseData);
-};
+    fetch(target_url, requestOptions)
+        .then(response => response.text())
+        .then(result => responseFunction(JSON.parse(result)))
+        .catch(error => console.log('error', error));
+}
+
+const target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/getPemasukanFromID";
+
+const dataPemasukan  = (value) => {
+    const data = formTodolist
+    .replace("#TANGGAL_MASUK#", value.title)
+    .replace("#JUMLAH_MASUK#", value.description)
+    .replace("#SUMBER#", value.deadline)
+    .replace("#DESKRIPSI#", value._id);
+
+    addInner("tablePemasukan", data);
+}
+
 
 const responseData = (result) => {
-    // console.log(result);
     if (result.status === true) {
+        result.data.forEach(dataPemasukan);
 
-        addInner("tanggal_masuk", result.data[0].tanggal_masuk)
-        addInner("jrupiah", result.data[0].rupiah)
-        addInner("sumber", result.data[0].sumber)
-        addInner("deskripsi", result.data[0].deskripsi)
+        console.log(result);
+    }
+}
 
-        document.getElementById("tanggal_masuk_Input").value = result.data[0].tanggal_masuk;
-        document.getElementById("rupiahInput").value = result.data[0].jumlah_masuk;
-        document.getElementById("sumber_Input").value = result.data[0].jumlah_masuk;
-        document.getElementById("deskripsi_Input").value = result.data[0].deskripsi;
+getWithToken(target_url, responseData);
+
+// const Postdata = () => {
+//     const target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/getAllPemasukan";
+
+//     let id = false;
+//     const urlParams = new URLSearchParams(window.location.search);
+//     if (urlParams.has('id')) {
+//         if (urlParams.get('id') != '') {
+//             id = urlParams.get('id');
+//         }
+//     }
+//     const datainjson = {
+//         "_id": id
+//     };
+//     const token = getCookie("token")
+//     if (token) {
+//         postWithBearer(target_url, token, datainjson, responseData)
+//     } else {
+//         console.log("token tidak ada " + result.message);
+//         alert("sesi anda sudah habis, silahkan logout dan login ulang")
+//         window.location.href = "pemasukan.html";
+
+//     }
+//     // post(target_url, datainjson, responseData);
+// };
+
+// const responseData = (result) => {
+//     // console.log(result);
+//     if (result.status === true) {
+
+//         addInner("tanggal_masuk", result.data[0].tanggal_masuk)
+//         addInner("jrupiah", result.data[0].rupiah)
+//         addInner("sumber", result.data[0].sumber)
+//         addInner("deskripsi", result.data[0].deskripsi)
+
+//         document.getElementById("tanggal_masuk_Input").value = result.data[0].tanggal_masuk;
+//         document.getElementById("rupiahInput").value = result.data[0].jumlah_masuk;
+//         document.getElementById("sumber_Input").value = result.data[0].jumlah_masuk;
+//         document.getElementById("deskripsi_Input").value = result.data[0].deskripsi;
         
 
 
-        const defaultValue = result.data[0].source.source; // Change this value as needed
-        document.querySelector(`input[type="radio"][value="${defaultValue}"]`).checked = true;
+//         const defaultValue = result.data[0].source.source; // Change this value as needed
+//         document.querySelector(`input[type="radio"][value="${defaultValue}"]`).checked = true;
 
-        //isi tabel
-        if (result.data[0].status == "inputting") {
+//         //isi tabel
+//         if (result.data[0].status == "inputting") {
 
-            document.getElementById("actionButton").innerHTML = "Tambah Pemasukan";
-            document.getElementById("actionButton").setAttribute("onclick", "tambah()");
-            document.getElementById("actionButton").setAttribute("class", "button is-primary");
+//             document.getElementById("actionButton").innerHTML = "Tambah Pemasukan";
+//             document.getElementById("actionButton").setAttribute("onclick", "tambah()");
+//             document.getElementById("actionButton").setAttribute("class", "button is-primary");
 
-            if (result.datapemasukan.length > 0) {
-                let index = 0;
-                let isiRow = (value) => {
-                    console.log(value)
-                    let content =
-                        tabelTopic.replace("#NO#", index += 1)
-                            .replace("#TANGGAL#", value.tanggal_masuk)
-                            .replace("#JUMLAH#", value.jumlah_masuk)
-                            .replace("#SUMBER#", value.sumber)
-                            .replace("#DESKRIPSI#", value.deskripsi)
-                    addInner("isiTabel", content);
-                }
+//             if (result.datapemasukan.length > 0) {
+//                 let index = 0;
+//                 let isiRow = (value) => {
+//                     console.log(value)
+//                     let content =
+//                         tabelTopic.replace("#NO#", index += 1)
+//                             .replace("#TANGGAL#", value.tanggal_masuk)
+//                             .replace("#JUMLAH#", value.jumlah_masuk)
+//                             .replace("#SUMBER#", value.sumber)
+//                             .replace("#DESKRIPSI#", value.deskripsi)
+//                     addInner("isiTabel", content);
+//                 }
 
-                result.datatopics.forEach(isiRow)
+//                 result.datatopics.forEach(isiRow)
 
-                document.getElementById("textNoData").style.display = "none";
-            }
-        } else if(result.data[0].status == "Tambah") {
-            document.getElementById("actionButton").innerHTML = "Data Pemasukan";
-            document.getElementById("yourElementId").removeAttribute("onclick");
-            document.getElementById("actionButton").setAttribute("class", "button is-primary");
-            document.getElementById("actionButton").setAttribute("disabled", true);
-        }
+//                 document.getElementById("textNoData").style.display = "none";
+//             }
+//         } else if(result.data[0].status == "Tambah") {
+//             document.getElementById("actionButton").innerHTML = "Data Pemasukan";
+//             document.getElementById("yourElementId").removeAttribute("onclick");
+//             document.getElementById("actionButton").setAttribute("class", "button is-primary");
+//             document.getElementById("actionButton").setAttribute("disabled", true);
+//         }
 
-        // window.location.href = "sentimen.html";
-    } else {
-        console.log(result.message);
-        alert(`parameter bermasalah atau sesi anda sudah habis, silahkan ulangi atau logout dan login ulang`);
-        window.location.href = "pemasukan.html";
+//         // window.location.href = "sentimen.html";
+//     } else {
+//         console.log(result.message);
+//         alert(`parameter bermasalah atau sesi anda sudah habis, silahkan ulangi atau logout dan login ulang`);
+//         window.location.href = "pemasukan.html";
 
-    }
-};
+//     }
+// };
 
-Postdata();
+// Postdata();
--- assets/js/insertpemasukan.js
+++ assets/js/insertpemasukan.js
@@ -1,62 +1,100 @@
 import { postWithBearer } from "../utilities/api.js";
-import getCookie from "https://jscroot.github.io/cookie/croot.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 import { getValue } from "https://jscroot.github.io/element/croot.js"
 
-const PostinsertPemasukan = () => {
-    const radioButtons = document.querySelectorAll('input[name="radioOption"]');
-    let selectedValue;
+const insertPemasukan = () => {
+    const target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/insertPemasukan"
+    const tokenkey = "Authorization";
+    const tokenvalue = getCookie("Authorization");
 
-    radioButtons.forEach(radioButton => {
-        if (radioButton.checked) {
-            selectedValue = radioButton.value;
-        }
-    });
-
-    //validasi form
-    if (getValue("tanggal_masuk_input") === ""|| getValue("tanggal_masuk_input") === null) {
-        alert("Tanggal tidak boleh kosong");
-        return;
-    }else if (getValue("rupiahInput") === ""|| getValue("rupiahInput") === null) {
-        alert("Jumlah masuk tidak boleh kosong");
-        return;
-    }else if (getValue("sumber_input") === ""|| getValue("sumber_input") === null) {
-        alert("Sumber tidak boleh kosong");
-        return;
-    }else if (getValue("deskripsi_input") === ""|| getValue("deskripsi_input") === null) {
-        alert("Deskripsi tidak boleh kosong");
-        return;
-    } else if (selectedValue === "" || selectedValue === null) {
-        alert("Deskripsi tidak boleh kosong");
-        return;
+    const data = {
+        "tanggal_masuk": getValue("tanggal_masuk"),
+        "jumlah_masuk": getValue("jumlah_masuk"),
+        "sumber": getValue("sumber"),
+        "deskripsi": getValue("deskripsi"),
     }
 
-    const target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/insertPemasukan";
-    const datainjson = {
-        tanggal_masuk: getValue("tanggal_masuk_input"),
-        source: {
-            source: selectedValue,
-            value: getValue("tanggal_masuk_input")
-        }
-    };
-    const token = getCookie("token")
-    if (token) {
-        postWithBearer(target_url, token, datainjson, responseData)
-        alert("Berhasil menambahkan topik")
-    } else {
-        console.log("token tidak ada " + result.message);
-        alert("sesi anda sudah habis, silahkan logout dan login ulang")
-    }
-};
+    postWithBearer(target_url, tokenkey, tokenvalue, data, responseData)
+}
 
 const responseData = (result) => {
-    // console.log(result);
     if (result.status === true) {
-        window.location.href = "pemasukan.html";
+        Swal.fire({
+            icon: "success",
+            title: "Insert Successful",
+            text: result.message,
+        }).then(() => {
+            window.location.href = "list_kegiatan.html";
+        });
     } else {
-        console.log(result.message);
-        alert(`sesi anda sudah habis, silahkan logout dan login ulang`);
+        Swal.fire({
+            icon: "error",
+            title: "Insert Failed",
+            text: result.message,
+        });
     }
-};
+}
+
+const btnInsert = document.getElementById("btnInsert");
+
+btnInsert.addEventListener("click", insertPemasukan);
 
 
-export default PostinsertPemasukan;
+// const PostinsertPemasukan = () => {
+//     const radioButtons = document.querySelectorAll('input[name="radioOption"]');
+//     let selectedValue;
+
+//     radioButtons.forEach(radioButton => {
+//         if (radioButton.checked) {
+//             selectedValue = radioButton.value;
+//         }
+//     });
+
+//     //validasi form
+//     if (getValue("tanggal_masuk_input") === ""|| getValue("tanggal_masuk_input") === null) {
+//         alert("Tanggal tidak boleh kosong");
+//         return;
+//     }else if (getValue("rupiahInput") === ""|| getValue("rupiahInput") === null) {
+//         alert("Jumlah masuk tidak boleh kosong");
+//         return;
+//     }else if (getValue("sumber_input") === ""|| getValue("sumber_input") === null) {
+//         alert("Sumber tidak boleh kosong");
+//         return;
+//     }else if (getValue("deskripsi_input") === ""|| getValue("deskripsi_input") === null) {
+//         alert("Deskripsi tidak boleh kosong");
+//         return;
+//     } else if (selectedValue === "" || selectedValue === null) {
+//         alert("Deskripsi tidak boleh kosong");
+//         return;
+//     }
+
+//     const target_url = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/insertPemasukan";
+//     const datainjson = {
+//         tanggal_masuk: getValue("tanggal_masuk_input"),
+//         source: {
+//             source: selectedValue,
+//             value: getValue("tanggal_masuk_input")
+//         }
+//     };
+//     const token = getCookie("token")
+//     if (token) {
+//         postWithBearer(target_url, token, datainjson, responseData)
+//         alert("Berhasil menambahkan topik")
+//     } else {
+//         console.log("token tidak ada " + result.message);
+//         alert("sesi anda sudah habis, silahkan logout dan login ulang")
+//     }
+// };
+
+// const responseData = (result) => {
+//     // console.log(result);
+//     if (result.status === true) {
+//         window.location.href = "pemasukan.html";
+//     } else {
+//         console.log(result.message);
+//         alert(`sesi anda sudah habis, silahkan logout dan login ulang`);
+//     }
+// };
+
+
+// export default PostinsertPemasukan;
