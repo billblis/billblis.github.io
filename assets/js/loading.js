8be6cafc87d7571d6605c3068d71f998043066b2
Fedhira Syaila <pedirasyaila@gmail.com>
2023-12-15 20:25:02 +0700

    update loading

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,32 +1,30 @@
-// new-loading.js
+// awesome-loading.js
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Create a new div for the new loader wrapper
-    const newLoaderWrapper = document.createElement("div");
-    newLoaderWrapper.id = "new-loader-wrapper";
-  
-    // Create a div for the new loader content
-    const newLoaderContent = document.createElement("div");
-    newLoaderContent.id = "new-loader";
-  
-    // Append the new loader content to the new loader wrapper
-    newLoaderWrapper.appendChild(newLoaderContent);
-  
-    // Append the new loader wrapper to the body
-    document.body.appendChild(newLoaderWrapper);
-  
-    // Function to show the new loading animation
-    function showNewLoading() {
-      newLoaderWrapper.style.display = "flex"; // Show the new loader wrapper
+    const awesomeLoaderWrapper = document.createElement("div");
+    awesomeLoaderWrapper.classList.add("awesome-loader-wrapper");
+
+    const awesomeLoaderContent = document.createElement("div");
+    awesomeLoaderContent.classList.add("awesome-loader");
+
+    for (let i = 0; i < 12; i++) {
+        const circle = document.createElement("div");
+        awesomeLoaderContent.appendChild(circle);
     }
-  
-    // Function to hide the new loading animation
-    function hideNewLoading() {
-      newLoaderWrapper.style.display = "none"; // Hide the new loader wrapper
+
+    awesomeLoaderWrapper.appendChild(awesomeLoaderContent);
+    document.body.appendChild(awesomeLoaderWrapper);
+
+    function showAwesomeLoading() {
+        document.body.style.overflow = "hidden"; // Prevent scrolling while loading
+        awesomeLoaderWrapper.style.display = "flex";
     }
-  
-    // Example: Show the new loading animation for 3 seconds (3000 milliseconds)
-    showNewLoading();
-    setTimeout(hideNewLoading, 3000);
-  });
-  
+
+    function hideAwesomeLoading() {
+        document.body.style.overflow = ""; // Enable scrolling after loading
+        awesomeLoaderWrapper.style.display = "none";
+    }
+
+    showAwesomeLoading();
+    setTimeout(hideAwesomeLoading, 3000);
+});
