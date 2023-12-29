2cd6d715a04ca9a5c35e399fbc39290fb0ef79e7
Fedhira Syaila <pedirasyaila@gmail.com>
2023-12-29 08:30:41 +0700

    update tabel laporan

---
+++ assets/js/getPemasukanCount.js
@@ -0,0 +1,31 @@
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+
+export const target_url_pemasukan = "https://asia-southeast2-xenon-hawk-402203.cloudfunctions.net/getAllPemasukan";
+
+const pemasukanCount = (count) => {
+    const resultCountElement = document.getElementById("pemasukanCount");
+    resultCountElement.innerHTML = `<td class="px-4 py-3 text-sm">${count}</td>`;  
+  };
+
+  const getPemasukan = (target_url) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", getCookie("Authorization"));
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    };
+
+    fetch(target_url, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      const jsonData = JSON.parse(result);
+
+      const pemasukancount = jsonData.length;
+
+      pemasukanCount(pemasukancount);
+    })
+    .catch((error) => console.log("error", error));
+};
+getPemasukan(target_url_pemasukan);
+  
