8fd1cbb53e597be2826f4b7321b4c0b3be93de10
Marlina Lubis <lubismarlina5@gmail.com>
2024-01-30 08:48:40 +0700

    update

--- assets/js/charts-pie.js
+++ assets/js/charts-pie.js
@@ -1,27 +1,27 @@
 
-// export const setPieChart = (pemasukan, pengeluaran) => {
-//   const pieConfig = {
-//     type: 'doughnut',
-//     data: {
-//       datasets: [
-//         {
-//           data: [pemasukan, pengeluaran],
-//           backgroundColor: ['#1c64f2', ' #dc3545'],
-//           label: 'Dataset 1',
-//         },
-//       ],
-//       labels: ['Pemasukan', 'Pengeluaran'],
-//     },
-//     options: {
-//       responsive: true,
-//       cutoutPercentage: 80,
-//       legend: {
-//         display: false,
-//       },
-//     },
-//   }
+export const setPieChart = (pemasukan, pengeluaran) => {
+  const pieConfig = {
+    type: 'doughnut',
+    data: {
+      datasets: [
+        {
+          data: [pemasukan, pengeluaran],
+          backgroundColor: ['#1c64f2', ' #dc3545'],
+          label: 'Dataset 1',
+        },
+      ],
+      labels: ['Pemasukan', 'Pengeluaran'],
+    },
+    options: {
+      responsive: true,
+      cutoutPercentage: 80,
+      legend: {
+        display: false,
+      },
+    },
+  }
 
-//   const pieCtx = document.getElementById('pie')
-//   window.myPie = new Chart(pieCtx, pieConfig)
-// }
+  const pieCtx = document.getElementById('pie')
+  window.myPie = new Chart(pieCtx, pieConfig)
+}
 
